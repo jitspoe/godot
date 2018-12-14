d893d70bee1c3146ebde012410639fb954f24f81
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-12-14 14:03:20 -0800

    Cleanup code and cleanup nodes.

--- modules/assimp/editor_scene_importer_asset_import.cpp
+++ modules/assimp/editor_scene_importer_asset_import.cpp
@@ -281,56 +281,7 @@ Spatial *EditorSceneImporterAssetImport::_generate_scene(const String &p_path, c
 			}
 			skeletons[i]->set_bone_parent(j, node_parent_index);
 		}
-
-		//for (Set<String>::Element *E = bone_split_names.front(); E; E = E->next()) {
-		//	int32_t bone_idx = skeletons[i]->find_bone(E->get());
-		//	aiNode *current_node = scene->mRootNode->FindNode(_string_to_ai_string(E->get()));
-		//	//skeletons[i]->set_bone_rest(bone_idx, Transform());
-		//}
 		skeletons[i]->localize_rests();
-	}
-	if (0) {
-		for (Map<Skeleton *, MeshInstance *>::Element *E = skeleton_meshes.front(); E; E = E->next()) {
-			// Armature is defined as the bone's skeleton root's parent node
-			Skeleton *s = E->key();
-			String root_bone_name;
-			if (s->get_bone_count() == 0) {
-				continue;
-			}
-			int32_t i = 0;
-			aiNode *current_node = scene->mRootNode->FindNode(_string_to_ai_string(s->get_bone_name(s->get_bone_count() - 1)));
-			while (current_node != NULL) {
-				if (bone_names.find(_ai_string_to_string(current_node->mName)) == NULL) {
-					break;
-				}
-				root_bone_name = _ai_string_to_string(current_node->mName);
-				current_node = scene->mRootNode->FindNode(current_node->mName)->mParent;
-			}
-
-			if (current_node == NULL) {
-				continue;
-			}
-
-			String armature_node_name = _ai_string_to_string(scene->mRootNode->FindNode(_string_to_ai_string(root_bone_name))->mParent->mName);
-			Spatial *armature_node = Object::cast_to<Spatial>(root->find_node(armature_node_name));
-
-			if (armature_node == NULL) {
-				continue;
-			}
-
-			//Transform armature_xform = armature_node->get_transform();
-			MeshInstance *mi = E->get();
-
-			//mi->get_parent()->remove_child(mi);
-			//armature_node->add_child(mi);
-			//mi->set_owner(root);
-
-			//s->get_parent()->remove_child(s);
-			//armature_node->add_child(s);
-			//s->set_owner(root);
-			mi->set_skeleton_path(mi->get_path_to(s));
-			mi->set_transform(armature_node->get_transform() * mi->get_transform());
-		}
 	}
 
 	const bool is_clear_bones = true;
@@ -761,6 +712,7 @@ void EditorSceneImporterAssetImport::_generate_node(const String &p_path, const 
 
 	parent->add_child(node);
 	node->set_transform(_extract_ai_matrix_transform(p_node->mTransformation, p_scale));
+
 	node->set_owner(p_owner);
 	bool has_uvs = false;
 	if (node->get_name() == _ai_string_to_string(p_scene->mRootNode->mName)) {
@@ -768,14 +720,14 @@ void EditorSceneImporterAssetImport::_generate_node(const String &p_path, const 
 
 		p_skeletons.push_back(s);
 	}
-	
+
 	for (size_t k = 0; k < p_skeletons.size(); k++) {
 		Skeleton *s = p_skeletons[k];
 		bool can_create_bone = node->get_name() != _ai_string_to_string(p_scene->mRootNode->mName) && p_node->mNumMeshes == 0;
 		if (can_create_bone) {
 			s->add_bone(node->get_name());
 			int32_t idx = s->find_bone(node->get_name());
-			Transform bone_offset = _get_global_ai_node_transform(p_scene, p_node,p_scale);
+			Transform bone_offset = _get_global_ai_node_transform(p_scene, p_node, p_scale);
 			s->set_bone_rest(idx, bone_offset);
 		}
 		p_parent->add_child(s);
@@ -828,7 +780,7 @@ bool EditorSceneImporterAssetImport::_generate_mesh_instance(const aiNode *p_nod
 
 	MeshInstance *mi = memnew(MeshInstance);
 	Node *parent_node = node->get_parent();
-	//parent_node->remove_child(node);
+	parent_node->remove_child(node);
 	parent_node->add_child(mi);
 	mi->set_owner(p_owner);
 	parent_node->add_child(s);
@@ -995,7 +947,6 @@ bool EditorSceneImporterAssetImport::_generate_mesh_instance(const aiNode *p_nod
 	s->set_name(String(mi->get_name()) + TTR("Skeleton"));
 	mi->set_skeleton_path(mi->get_path_to(s));
 	Transform orient_xform;
-
 	mi->set_mesh(mesh);
 	//for (int i = 0; i < mesh.blend_weights.size(); i++) {
 	//	mi->set("blend_shapes/" + mesh.mesh->get_blend_shape_name(i), mesh.blend_weights[i]);

ab6c89fec839fbeb96c371a66f70535907a8ce8b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-12-16 17:17:42 -0800

    WIP. Add more error messages.

--- modules/assimp/editor_scene_importer_asset_import.h
+++ modules/assimp/editor_scene_importer_asset_import.h
@@ -65,14 +65,14 @@ private:
 
 	Spatial *_generate_scene(const String &p_path, const aiScene *scene, const uint32_t p_flags, int p_bake_fps);
 
-	Transform _get_armature_xform(const aiScene *scene, const Skeleton *s, const Map<String, int32_t> bone_names, const Spatial *root);
+	Transform _get_armature_xform(const aiScene *scene, const Skeleton *s, const Set<String> bone_names, const Spatial *root);
 
 	Transform _get_global_ai_node_transform(const aiScene *p_scene, const aiNode *p_current_node);
-	void _generate_node(const String &p_path, const aiScene *p_scene, const aiNode *p_node, Node *p_parent, Node *p_owner, Vector<Skeleton *> &p_skeletons, Map<String, int32_t> &r_bone_name, Set<String> p_light_names, Set<String> p_camera_names);
+	void _generate_node(const String &p_path, const aiScene *p_scene, const aiNode *p_node, Node *p_parent, Node *p_owner, Vector<Skeleton *> &p_skeletons, Set<String> & r_bone_name, Set<String> p_light_names, Set<String> p_camera_names);
 	aiString _string_to_ai_string(String bone_name);
 	void _insert_animation_track(const aiScene *p_scene, int p_bake_fps, Ref<Animation> animation, float ticks_per_second, float length, Skeleton *sk, size_t i, const aiNodeAnim *track, String node_name, NodePath node_path);
 	bool is_part_of_split_mesh(Set<String> &p_bone_split_names, String node_name);
-	bool _add_mesh_to_mesh_instance(const aiNode *p_node, const aiScene *p_scene, bool has_uvs, Skeleton *s, const String &p_path, MeshInstance *p_mesh_instance, Node *p_owner, Map<String, int32_t> &r_bone_name);
+	bool _add_mesh_to_mesh_instance(const aiNode *p_node, const aiScene *p_scene, bool has_uvs, Skeleton *s, const String &p_path, MeshInstance *p_mesh_instance, Node *p_owner, Set<String>& r_bone_name);
 	void _load_material_type(SpatialMaterial::TextureParam p_spatial_material_type, aiTextureType p_texture_type, Ref<SpatialMaterial> p_spatial_material, aiMaterial *p_ai_material, const String &p_path);
 	bool _find_texture_path(const String &p_path, _Directory &dir, String &path, bool &found, String extension);
 	String _ai_string_to_string(const aiString p_node);

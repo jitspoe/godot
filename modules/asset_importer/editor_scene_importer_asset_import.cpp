13bd574acb23ec51548e5f3d280704bb167890a7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-12-31 17:43:58 -0800

    Find those textures!

--- modules/asset_importer/editor_scene_importer_asset_import.cpp
+++ modules/asset_importer/editor_scene_importer_asset_import.cpp
@@ -1135,21 +1135,21 @@ bool EditorSceneImporterAssetImport::_find_texture_path(const String &p_path, _D
 	}
 
 	String name_find_texture_sub_directory = p_path.get_base_dir() + "/textures/" + path.get_file().get_basename() + extension;
-	if (dir.file_exists(name_ignore_sub_directory)) {
+	if (dir.file_exists(name_find_texture_sub_directory)) {
 		found = true;
-		path = name_ignore_sub_directory;
+		path = name_find_texture_sub_directory;
 		return found;
 	}
 	String name_find_texture_upper_sub_directory = p_path.get_base_dir() + "/Textures/" + path.get_file().get_basename() + extension;
-	if (dir.file_exists(name_ignore_sub_directory)) {
+	if (dir.file_exists(name_find_texture_upper_sub_directory)) {
 		found = true;
-		path = name_ignore_sub_directory;
+		path = name_find_texture_upper_sub_directory;
 		return found;
 	}
-	String name_find_texture_outside_sub_directory = p_path.get_base_dir() + "../textures/" + path.get_file().get_basename() + extension;
-	if (dir.file_exists(name_ignore_sub_directory)) {
+	String name_find_texture_outside_sub_directory = p_path.get_base_dir() + "/../textures/" + path.get_file().get_basename() + extension;
+	if (dir.file_exists(name_find_texture_outside_sub_directory)) {
 		found = true;
-		path = name_ignore_sub_directory;
+		path = name_find_texture_outside_sub_directory;
 		return found;
 	}
 

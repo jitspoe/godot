bb857ce051a38490dfa1d7baa377282314dd280e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-01-15 01:20:47 -0800

    Walk through alembic file.

--- thirdparty/assimp/code/ABCImporter.h
+++ thirdparty/assimp/code/ABCImporter.h
@@ -39,15 +39,63 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ----------------------------------------------------------------------
 */
+
+//-*****************************************************************************
+//
+// Copyright (c) 2013,
+//  Sony Pictures Imageworks, Inc. and
+//  Industrial Light & Magic, a division of Lucasfilm Entertainment Company Ltd.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// *       Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// *       Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+// *       Neither the name of Sony Pictures Imageworks, nor
+// Industrial Light & Magic nor the names of their contributors may be used
+// to endorse or promote products derived from this software without specific
+// prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//-*****************************************************************************
+
 #ifndef INCLUDED_AI_ABC_IMPORTER_H
 #define INCLUDED_AI_ABC_IMPORTER_H
 
 #include <assimp/BaseImporter.h>
 
+#include <Alembic/AbcCollection/All.h>
+#include <Alembic/AbcCoreFactory/All.h>
+#include <Alembic/AbcCoreOgawa/All.h>
+#include <Alembic/AbcGeom/All.h>
+#include <Alembic/AbcMaterial/All.h>
+
 #include <set>
 #include <string>
 
 namespace Assimp {
+
+namespace Abc = ::Alembic::Abc;
+namespace AbcA = ::Alembic::AbcCoreAbstract;
+namespace AbcF = ::Alembic::AbcCoreFactory;
+namespace AbcG = ::Alembic::AbcGeom;
 
 class ABCImporter : public BaseImporter {
 public:
@@ -56,9 +104,16 @@ public:
 
 protected:
 	const aiImporterDesc *GetInfo() const;
-	void ABCImporter::GetExtensionList(std::set<std::string> &extensions);
-	void ABCImporter::InternReadFile(const std::string &pFile,
+	void GetExtensionList(std::set<std::string> &extensions);
+	void InternReadFile(const std::string &pFile,
 			aiScene *pScene, IOSystem *pIOHandler);
+	bool is_leaf(AbcG::IObject iObj);
+	bool is_leaf(Abc::ICompoundProperty iProp, Abc::PropertyHeader iHeader);
+	int index(Abc::ICompoundProperty iProp, Abc::PropertyHeader iHeader);
+	void tree(Abc::IScalarProperty iProp, std::string prefix = "");
+	void tree(Abc::IArrayProperty iProp, std::string prefix = "");
+	void tree(Abc::ICompoundProperty iProp, std::string prefix = "");
+	void tree(AbcG::IObject iObj, bool showProps = false, std::string prefix = "");
 };
 }
 #endif

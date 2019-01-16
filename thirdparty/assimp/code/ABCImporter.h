1278e3a9fd549fcc8210c28c8077894eaa46467b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-01-15 23:57:36 -0800

    Cleanup.

--- thirdparty/assimp/code/ABCImporter.h
+++ thirdparty/assimp/code/ABCImporter.h
@@ -110,6 +110,7 @@ protected:
 	void GetExtensionList(std::set<std::string> &extensions);
 	void InternReadFile(const std::string &pFile,
 			aiScene *pScene, IOSystem *pIOHandler);
+private:
 	bool is_leaf(AbcG::IObject iObj);
 	bool is_leaf(Abc::ICompoundProperty iProp, Abc::PropertyHeader iHeader);
 	int index(Abc::ICompoundProperty iProp, Abc::PropertyHeader iHeader);
@@ -117,74 +118,9 @@ protected:
 	void tree(Abc::IArrayProperty iProp, aiScene *pScene, std::string prefix = "");
 	void tree(Abc::ICompoundProperty iProp, aiScene *pScene, std::string prefix = "");
 	void tree(AbcG::IObject iObj, aiScene *pScene, aiNode *current, bool showProps = false, std::string prefix = "");
-
-private:
-	void TransferDataToScene(aiScene *pScene) {
-		ai_assert(!pScene->mMeshes);
-		ai_assert(!pScene->mNumMeshes);
-
-		// note: the trailing () ensures initialization with NULL - not
-		// many C++ users seem to know this, so pointing it out to avoid
-		// confusion why this code works.
-
-		if (meshes.size()) {
-			pScene->mMeshes = new aiMesh *[meshes.size()]();
-			pScene->mNumMeshes = static_cast<unsigned int>(meshes.size());
-
-			std::swap_ranges(meshes.begin(), meshes.end(), pScene->mMeshes);
-		}
-
-		if (materials.size()) {
-			pScene->mMaterials = new aiMaterial *[materials.size()]();
-			pScene->mNumMaterials = static_cast<unsigned int>(materials.size());
-
-			std::swap_ranges(materials.begin(), materials.end(), pScene->mMaterials);
-		}
-
-		if (animations.size()) {
-			pScene->mAnimations = new aiAnimation *[animations.size()]();
-			pScene->mNumAnimations = static_cast<unsigned int>(animations.size());
-
-			std::swap_ranges(animations.begin(), animations.end(), pScene->mAnimations);
-		}
-
-		if (lights.size()) {
-			pScene->mLights = new aiLight *[lights.size()]();
-			pScene->mNumLights = static_cast<unsigned int>(lights.size());
-
-			std::swap_ranges(lights.begin(), lights.end(), pScene->mLights);
-		}
-
-		if (cameras.size()) {
-			pScene->mCameras = new aiCamera *[cameras.size()]();
-			pScene->mNumCameras = static_cast<unsigned int>(cameras.size());
-
-			std::swap_ranges(cameras.begin(), cameras.end(), pScene->mCameras);
-		}
-
-		if (textures.size()) {
-			pScene->mTextures = new aiTexture *[textures.size()]();
-			pScene->mNumTextures = static_cast<unsigned int>(textures.size());
-
-			std::swap_ranges(textures.begin(), textures.end(), pScene->mTextures);
-		}
-	}
-	aiMesh *SetupEmptyMesh(AbcG::IPolyMesh &polymesh, const std::string &facesetName, aiNode *node) {
-		// https://github.com/alembic/alembic/blob/8bf5f3494ead3ccb8820fd1768d3f8c764e43fb9/arnold/Procedural/WriteGeo.cpp#L515
-		// FBXConverter.cpp
-
-		aiMesh *const out_mesh = new aiMesh();
-		meshes.push_back(out_mesh);
-		// set name
-		std::string name = facesetName;
-
-		if (name.length()) {
-			out_mesh->mName.Set(name);
-		} else {
-			out_mesh->mName = node->mName;
-		}
-		return out_mesh;
-	}
+	void ConvertMeshSingleMaterial(IPolyMesh polymesh, std::string faceSetName, aiNode *current);
+	void TransferDataToScene(aiScene *pScene);
+	aiMesh *SetupEmptyMesh(AbcG::IPolyMesh &polymesh, const std::string &facesetName, aiNode *node);
 
 	std::vector<aiMesh *> meshes;
 	std::vector<aiMaterial *> materials;

520df018a13b28c785218d913f2f00800a12c10c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-01-28 07:54:32 -0800

    More determinant and orthogonal basis testing.

--- modules/asset_import/editor_scene_importer_asset_import.cpp
+++ modules/asset_import/editor_scene_importer_asset_import.cpp
@@ -535,13 +535,17 @@ void EditorSceneImporterAssetImport::_insert_animation_track(const aiScene *p_sc
 				xform.origin = pos;
 
 				int bone = sk->find_bone(node_name);
-				Transform rest_xform = sk->get_bone_rest(bone).affine_inverse();
-				xform = rest_xform * xform;
-				
-				rot = xform.basis.get_quat();
-				rot.normalize();
-				scale = xform.basis.get_scale();
-				pos = xform.origin;
+				Transform rest_xform = sk->get_bone_rest(bone);
+				if (Math:: is_equal_approx(rest_xform.basis.determinant(), 0.0f) == false) {
+					rest_xform.orthonormalize();
+					xform = rest_xform.affine_inverse() * xform;
+					if (Math::is_equal_approx(xform.basis.determinant(), 0.0f) == false && xform.basis.is_orthogonal()) {
+						rot = xform.basis.get_quat();
+						rot.normalize();
+						scale = xform.basis.get_scale();
+						pos = xform.origin;
+					}
+				}
 			}
 			if (p_skeleton_root.empty() == false) {
 				Transform anim_xform;

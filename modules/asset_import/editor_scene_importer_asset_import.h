aa1c249b1e25bb6224ab1a3c9c372ffd8e3e63ec
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-02-01 04:50:03 -0800

    Embedded textures appear to work.

--- modules/asset_import/editor_scene_importer_asset_import.h
+++ modules/asset_import/editor_scene_importer_asset_import.h
@@ -152,7 +152,7 @@ private:
 	void _get_track_set(const aiScene *p_scene, Set<String> &tracks);
 	void _insert_animation_track(const aiScene *p_scene, const String p_path, int p_bake_fps, Ref<Animation> animation, float ticks_per_second, float length, const Skeleton *sk, size_t i, const aiNodeAnim *track, String node_name, String p_skeleton_root, NodePath node_path);
 	void _add_mesh_to_mesh_instance(const aiNode *p_node, const aiScene *p_scene, Skeleton *s, const String &p_path, MeshInstance *p_mesh_instance, Node *p_owner, Set<String> &r_bone_name, int32_t &r_mesh_count);
-	Ref<Texture> _load_texture(const aiScene *p_scene, Ref<Texture> p_texture, String p_path);
+	Ref<Texture> _load_texture(const aiScene *p_scene, String p_path);
 	void _calc_tangent_from_mesh(const aiMesh *ai_mesh, int i, int tri_index, int index, PoolColorArray::Write &w);
 	void _set_texture_mapping_mode(aiTextureMapMode *map_mode, Ref<Texture> texture);
 	void _find_texture_path(const String &p_path, String &path, bool &r_found);

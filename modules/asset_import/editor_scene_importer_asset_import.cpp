7d088944ed9d44a82693e976e9f9f5f399999f27
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-02-19 10:49:08 -0800

    Name F better as p_node_animation.

--- modules/asset_import/editor_scene_importer_asset_import.cpp
+++ modules/asset_import/editor_scene_importer_asset_import.cpp
@@ -1069,7 +1069,7 @@ void EditorSceneImporterAssetImport::_import_animation(const String p_path, cons
 	}
 }
 
-void EditorSceneImporterAssetImport::_insert_pivot_anim_track(const Map<MeshInstance *, String> p_meshes, const Map<MeshInstance *, String> p_orig_meshes, const String p_node_name, Vector<const aiNodeAnim *> F, AnimationPlayer *ap, Skeleton *sk, float &length, float ticks_per_second, Ref<Animation> animation, int p_bake_fps, const String &p_path, const aiScene *p_scene) {
+void EditorSceneImporterAssetImport::_insert_pivot_anim_track(const Map<MeshInstance *, String> p_meshes, const Map<MeshInstance *, String> p_orig_meshes, const String p_node_name, Vector<const aiNodeAnim *> p_node_animations, AnimationPlayer *ap, Skeleton *sk, float &length, float ticks_per_second, Ref<Animation> animation, int p_bake_fps, const String &p_path, const aiScene *p_scene) {
 	NodePath node_path;
 	String p_skeleton_root;
 	String p_orig_skeleton_root;
@@ -1123,12 +1123,12 @@ void EditorSceneImporterAssetImport::_insert_pivot_anim_track(const Map<MeshInst
 	Quat base_rot;
 	Vector3 base_scale = Vector3(1, 1, 1);
 
-	for (size_t k = 0; k < F.size(); k++) {
+	for (size_t k = 0; k < p_node_animations.size(); k++) {
 		bool is_translation = false;
 		bool is_rotation = false;
 		bool is_scaling = false;
 
-		String p_track_type = _ai_string_to_string(F[k]->mNodeName).split(ASSIMP_FBX_KEY)[1];
+		String p_track_type = _ai_string_to_string(p_node_animations[k]->mNodeName).split(ASSIMP_FBX_KEY)[1];
 		if (p_track_type == "_Translation") {
 			is_translation = true;
 		} else if (p_track_type == "_Rotation") {
@@ -1140,21 +1140,21 @@ void EditorSceneImporterAssetImport::_insert_pivot_anim_track(const Map<MeshInst
 		}
 		ERR_CONTINUE(ap->get_owner()->has_node(node_path) == false);
 
-		if (F[k]->mNumRotationKeys || F[k]->mNumPositionKeys || F[k]->mNumScalingKeys) {
+		if (p_node_animations[k]->mNumRotationKeys || p_node_animations[k]->mNumPositionKeys || p_node_animations[k]->mNumScalingKeys) {
 
 			if (is_rotation) {
-				for (int i = 0; i < F[k]->mNumRotationKeys; i++) {
-					length = MAX(length, F[k]->mRotationKeys[i].mTime / ticks_per_second);
+				for (int i = 0; i < p_node_animations[k]->mNumRotationKeys; i++) {
+					length = MAX(length, p_node_animations[k]->mRotationKeys[i].mTime / ticks_per_second);
 				}
 			}
 			if (is_translation) {
-				for (int i = 0; i < F[k]->mNumPositionKeys; i++) {
-					length = MAX(length, F[k]->mPositionKeys[i].mTime / ticks_per_second);
+				for (int i = 0; i < p_node_animations[k]->mNumPositionKeys; i++) {
+					length = MAX(length, p_node_animations[k]->mPositionKeys[i].mTime / ticks_per_second);
 				}
 			}
 			if (is_scaling) {
-				for (int i = 0; i < F[k]->mNumScalingKeys; i++) {
-					length = MAX(length, F[k]->mScalingKeys[i].mTime / ticks_per_second);
+				for (int i = 0; i < p_node_animations[k]->mNumScalingKeys; i++) {
+					length = MAX(length, p_node_animations[k]->mScalingKeys[i].mTime / ticks_per_second);
 				}
 			}
 
@@ -1163,8 +1163,8 @@ void EditorSceneImporterAssetImport::_insert_pivot_anim_track(const Map<MeshInst
 			}
 
 			if (is_rotation) {
-				if (F[k]->mNumRotationKeys != 0) {
-					aiQuatKey key = F[k]->mRotationKeys[0];
+				if (p_node_animations[k]->mNumRotationKeys != 0) {
+					aiQuatKey key = p_node_animations[k]->mRotationKeys[0];
 					real_t x = key.mValue.x;
 					real_t y = key.mValue.y;
 					real_t z = key.mValue.z;
@@ -1176,8 +1176,8 @@ void EditorSceneImporterAssetImport::_insert_pivot_anim_track(const Map<MeshInst
 			}
 
 			if (is_translation) {
-				if (F[k]->mNumPositionKeys != 0) {
-					aiVectorKey key = F[k]->mPositionKeys[0];
+				if (p_node_animations[k]->mNumPositionKeys != 0) {
+					aiVectorKey key = p_node_animations[k]->mPositionKeys[0];
 					real_t x = key.mValue.x;
 					real_t y = key.mValue.y;
 					real_t z = key.mValue.z;
@@ -1186,8 +1186,8 @@ void EditorSceneImporterAssetImport::_insert_pivot_anim_track(const Map<MeshInst
 			}
 
 			if (is_scaling) {
-				if (F[k]->mNumScalingKeys != 0) {
-					aiVectorKey key = F[k]->mScalingKeys[0];
+				if (p_node_animations[k]->mNumScalingKeys != 0) {
+					aiVectorKey key = p_node_animations[k]->mScalingKeys[0];
 					real_t x = key.mValue.x;
 					real_t y = key.mValue.y;
 					real_t z = key.mValue.z;
@@ -1195,26 +1195,26 @@ void EditorSceneImporterAssetImport::_insert_pivot_anim_track(const Map<MeshInst
 				}
 			}
 			if (is_translation) {
-				for (size_t p = 0; p < F[k]->mNumPositionKeys; p++) {
-					aiVector3D pos = F[k]->mPositionKeys[p].mValue;
+				for (size_t p = 0; p < p_node_animations[k]->mNumPositionKeys; p++) {
+					aiVector3D pos = p_node_animations[k]->mPositionKeys[p].mValue;
 					pos_values.push_back(Vector3(pos.x, pos.y, pos.z));
-					pos_times.push_back(F[k]->mPositionKeys[p].mTime / ticks_per_second);
+					pos_times.push_back(p_node_animations[k]->mPositionKeys[p].mTime / ticks_per_second);
 				}
 			}
 
 			if (is_rotation) {
-				for (size_t r = 0; r < F[k]->mNumRotationKeys; r++) {
-					aiQuaternion quat = F[k]->mRotationKeys[r].mValue;
+				for (size_t r = 0; r < p_node_animations[k]->mNumRotationKeys; r++) {
+					aiQuaternion quat = p_node_animations[k]->mRotationKeys[r].mValue;
 					rot_values.push_back(Quat(quat.x, quat.y, quat.z, quat.w).normalized());
-					rot_times.push_back(F[k]->mRotationKeys[r].mTime / ticks_per_second);
+					rot_times.push_back(p_node_animations[k]->mRotationKeys[r].mTime / ticks_per_second);
 				}
 			}
 
 			if (is_scaling) {
-				for (size_t sc = 0; sc < F[k]->mNumScalingKeys; sc++) {
-					aiVector3D scale = F[k]->mScalingKeys[sc].mValue;
+				for (size_t sc = 0; sc < p_node_animations[k]->mNumScalingKeys; sc++) {
+					aiVector3D scale = p_node_animations[k]->mScalingKeys[sc].mValue;
 					scale_values.push_back(Vector3(scale.x, scale.y, scale.z));
-					scale_times.push_back(F[k]->mScalingKeys[sc].mTime / ticks_per_second);
+					scale_times.push_back(p_node_animations[k]->mScalingKeys[sc].mTime / ticks_per_second);
 				}
 			}
 		}

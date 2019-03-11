220a660d4d3e0983ace74bdf65a9a95cb5b645a8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-03-11 10:28:27 -0700

    Preserve pivots.

--- modules/asset_import/editor_scene_importer_asset_import.cpp
+++ modules/asset_import/editor_scene_importer_asset_import.cpp
@@ -347,7 +347,8 @@ Node *EditorSceneImporterAssetImport::import_scene(const String &p_path, uint32_
 	std::wstring w_path = ProjectSettings::get_singleton()->globalize_path(p_path).c_str();
 	std::string s_path(w_path.begin(), w_path.end());
 	importer.SetPropertyBool(AI_CONFIG_PP_FD_REMOVE, true);
-	importer.SetPropertyBool(AI_CONFIG_IMPORT_FBX_PRESERVE_PIVOTS, false);
+	// Cannot remove pivot points because the static mesh is in the wrong place
+	importer.SetPropertyBool(AI_CONFIG_IMPORT_FBX_PRESERVE_PIVOTS, true);
 	importer.SetPropertyInteger(AI_CONFIG_PP_SBP_REMOVE, aiPrimitiveType_LINE | aiPrimitiveType_POINT);
 	//importer.SetPropertyFloat(AI_CONFIG_PP_DB_THRESHOLD, 1.0f);
 	int32_t post_process_Steps = aiProcess_CalcTangentSpace |

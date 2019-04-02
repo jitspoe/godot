28dae8b4a6efe722b24d2232770933b041b7adb3
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-04-02 08:59:12 -0700

    Remove the code. Animations look better.

--- modules/asset_import/editor_scene_importer_asset_import.cpp
+++ modules/asset_import/editor_scene_importer_asset_import.cpp
@@ -1148,18 +1148,18 @@ void EditorSceneImporterAssetImport::_insert_pivot_anim_track(const Vector<MeshI
 		Vector3 scale = Vector3(1.0f, 1.0f, 1.0f);
 		if (is_translation && pos_values.size()) {
 			pos = _interpolate_track<Vector3>(pos_times, pos_values, time, AssetImportAnimation::INTERP_LINEAR);
-			String ext = p_path.get_file().get_extension().to_lower();
-			if (ext == "fbx") {
-				aiNode *ai_node = _ai_find_node(p_scene->mRootNode, p_node_name);
-				Transform mesh_xform = _get_global_ai_node_transform(p_scene, ai_node);
-				pos = mesh_xform.origin + pos;
-			}
-			real_t factor = 1.0f;
-			if (p_scene->mMetaData != NULL) {
-				p_scene->mMetaData->Get("UnitScaleFactor", factor);
-				factor = factor * 0.01f;
-			}
-			pos = pos * factor;
+			//String ext = p_path.get_file().get_extension().to_lower();
+			//if (ext == "fbx") {
+			//	aiNode *ai_node = _ai_find_node(p_scene->mRootNode, p_node_name);
+			//	Transform mesh_xform = _get_global_ai_node_transform(p_scene, ai_node);
+			//	pos = mesh_xform.origin + pos;
+			//}
+			//real_t factor = 1.0f;
+			//if (p_scene->mMetaData != NULL) {
+			//	p_scene->mMetaData->Get("UnitScaleFactor", factor);
+			//	factor = factor * 0.01f;
+			//}
+			//pos = pos * factor;
 		}
 		if (is_rotation && rot_values.size()) {
 			rot = _interpolate_track<Quat>(rot_times, rot_values, time, AssetImportAnimation::INTERP_LINEAR).normalized();

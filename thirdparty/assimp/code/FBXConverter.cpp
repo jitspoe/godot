9da5fff2c5605bbd32fcf7220da6c7d9f663b98b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-06-10 09:38:14 -0700

    Assimp: Update to master.

--- thirdparty/assimp/code/FBXConverter.cpp
+++ thirdparty/assimp/code/FBXConverter.cpp
@@ -1479,7 +1479,7 @@ namespace Assimp {
                     // if we found at least one, generate the output bones
                     // XXX this could be heavily simplified by collecting the bone
                     // data in a single step.
-                    if (ok) {
+                    if (ok && mRemoveEmptyBones) {
                         ConvertCluster(bones, model, *cluster, out_indices, index_out_indices,
                             count_out_indices, node_global_transform);
                     }

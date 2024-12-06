dea7e772e880fc57e3e0b746fa0d3319016c0be2
jitspoe <jitspoe@yahoo.com>
2024-12-05 20:24:18 -0500

    Removed qodot.

--- modules/qodot/register_types.h
+++
@@ -1,3 +0,0 @@
-// Yes, the word in the middle must be the same as the module folder name
-void initialize_qodot_module(ModuleInitializationLevel p_level);
-void uninitialize_qodot_module(ModuleInitializationLevel p_level);
